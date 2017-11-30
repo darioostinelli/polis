27c5276e65358597e44b2b611129eb65e294faca
darioostinelli <daostinelli@gmail.com>
2017-11-30 10:01:04 +0100

    setup dashboard UI

---
+++ dashboard/mainPage.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Polis Dashboard</title>
+<link rel="stylesheet" type="text/css" href="/polis/styles/polis.css">
+<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+<script src="/polis/js/librerie/jquery-3.2.1.js"></script>
+<script src="/polis/js/componenti/login.js"></script>
+<script src="/polis/js/funzioni/login.js"></script>
+<link rel="shortcut icon" href="/polis/logo.ico" />
+</head>
+<body>
+	<div class="main-page">
+		<div class="menu">
+			<div class="logo-container">
+				<img src="/polis/src/img/logos/polis-logo.png" />
+				<div class="header only-mobile">
+					<div class="header-element">Logout</div>
+				</div>
+			</div>
+			<div class="menu-item">Elem</div>
+			<div class="menu-item">Elem</div>
+			<div class="menu-item">Elem</div>
+			<div class="menu-item">Elem</div>
+			<div class="menu-item">Elem</div>
+		</div>
+		<div class="dashboard">
+			<div class="header only-desktop">
+				<div class="header-element">Logout</div>
+			</div>
+			
+		</div>
+		
+	</div>
+	<div class="dashboard-footer">
+		Polis - IoT Solution<br>
+		Dario Ostinelli - 2018
+	</div>
+</body>
+</html>
+
+<?php
+?>
